705b574b31720af1e76e9bd902c34d02d830d480
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-03-23 22:59:54 +0700

    nested component & use  template

--- basic/src/app/app.component.ts
+++ basic/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ServerComponent],
+  imports: [RouterOutlet,ServerComponent,ServersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

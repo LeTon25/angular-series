0652677a9edaee79853eb46161ceb935d02c914b
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-03-23 23:36:21 +0700

    Data binding : property binding

--- basic/src/app/servers/servers.component.ts
+++ basic/src/app/servers/servers.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServerComponent } from '../server/server.component';
 @Component({
   selector: 'app-servers',
   standalone: true,
   imports: [ServerComponent],
-  template: '<app-server></app-server> <app-server></app-server> <app-server></app-server>',
+  // template: '<app-server></app-server> <app-server></app-server> <app-server></app-server>',
+  templateUrl :'./servers.component.html',
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
-
+export class ServersComponent  implements OnInit{
+  allowAddServer:boolean = false;
+  constructor(){
+    setTimeout(()=>{
+      this.allowAddServer = true
+    },200)
+  }
+  ngOnInit(): void {
+    
+  }
 }

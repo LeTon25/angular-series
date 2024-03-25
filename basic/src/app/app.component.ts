c0a692932b248b69e947531d3cfdf4c6d34d98cc
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-03-25 16:29:31 +0700

    create components

--- basic/src/app/app.component.ts
+++ basic/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ServerComponent,ServersComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

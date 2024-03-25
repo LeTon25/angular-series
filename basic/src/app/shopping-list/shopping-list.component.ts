469970dde5859010b9658f6584923d519559dcea
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-03-25 21:14:35 +0700

    finish

--- basic/src/app/shopping-list/shopping-list.component.ts
+++ basic/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { Ingredient } from '../shared/ingredient.model';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [ShoppingEditComponent],
+  imports: [ShoppingEditComponent,CommonModule],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients : Ingredient[] = [
+    new Ingredient("Apple",20),
+    new Ingredient("Coconut",10)
+  ]
+  constructor(){}
 
+  ngOnInit(): void {
+    
+  }
 }

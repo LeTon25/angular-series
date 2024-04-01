60ac0bac8d2b4166c8a8551ed659376970ed85b5
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-01 16:26:37 +0700

    add navigation feature

--- cmp/src/app/header/header.component.ts
+++ cmp/src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+  constructor(){}
+  
+  ngOnInit(): void {
+    
+  }
+  onSelect( $selectedItem:string): void{
+    this.featureSelected.emit($selectedItem)
+  }
 }

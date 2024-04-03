567769b6fa822992b659b2c4ae519069b6c93810
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-03 22:11:09 +0700

    add 2 service

--- start/src/app/shopping-list/shopping-list.service.ts
+++ start/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 export class ShoppingListService{
-    
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+    addIngredients(ingredient:Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice())
+    }
 }

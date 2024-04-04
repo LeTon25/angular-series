9250de8cf52f5d98ae94e84ce4b3125df8eadb73
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-04 15:34:56 +0700

    fix routing in server component

--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -12,9 +13,11 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe();
+    this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
--- routing/src/app/servers/servers.component.ts
+++ routing/src/app/servers/servers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ServersService } from './servers.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ServersService } from './servers.service';
 
 @Component({
   selector: 'app-servers',

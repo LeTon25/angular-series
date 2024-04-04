290da4d24ba816bae9e94945bf602810814a03fa
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-04 17:12:39 +0700

    fake login service

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.serive';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,8 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,
+      private auth:AuthService) { }
 
   ngOnInit() {
   }
@@ -19,4 +21,11 @@ export class HomeComponent implements OnInit {
   onLoadServer1(id :number){
     this.router.navigate(['/servers',5,'edit'],{queryParams: {allowEdit:1},fragment:'loading'})
   }
+  onLogin(){
+    this.auth.logIn()
+  }
+  onLogout(){
+    this.auth.logOut()
+  }
 }
+

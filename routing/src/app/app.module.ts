0d2405beec8c7c75fa76bb5c5403a0cb7f8fc20f
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-04 16:33:08 +0700

    seperate a routing module file

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -13,20 +13,9 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingRoute } from './app-routing.module';
 
-const appRoutes:Routes = [
-  {path :'',component : HomeComponent},
-  {path :'users',component: UsersComponent,children:[
-    {path :':id/:name',component:UserComponent},
-  ]},
-  {path :'servers',component: ServersComponent,children:[
-    {path :':id',component: ServerComponent},
-    {path :':id/edit',component: EditServerComponent},
-  ]},
-  {path :'not-found',component:PageNotFoundComponent},
-  {path :'**', redirectTo:'/not-found'},
 
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,7 +30,7 @@ const appRoutes:Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingRoute
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]

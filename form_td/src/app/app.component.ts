974aaf95ea2d66dc963ab1d32320283bebc059d8
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-13 17:16:32 +0700

    finish

--- form_td/src/app/app.component.ts
+++ form_td/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') formSignUp:NgForm
+  defaultQuestion:string = 'pet'
+  answer:string;
+  genders=['male','female']
+  user ={
+    username :'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  }
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.formSignUp.form.patchValue({
+      userData:{
+        username:  suggestedName
+      }
+    })
+  }
+  // onSubmit(form:NgForm){
+  //   console.log(form)
+  // }
+  onSubmit(){
+    this.user.username = this.formSignUp.value.userData.username;
+    this.user.email = this.formSignUp.value.userData.email;
+    this.user.secretQuestion =  this.formSignUp.value.secret;
+    this.user.answer = this.formSignUp.value.questionAnswer;
+    this.user.gender = this.formSignUp.value.gender;  
+    this.submitted = true;
   }
 }

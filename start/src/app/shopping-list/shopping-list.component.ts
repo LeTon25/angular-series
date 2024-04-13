c8c12a6a9862da65bfee31d40880d77f5aac178b
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-13 21:05:53 +0700

    start

---
+++ start/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent implements OnInit,OnDestroy {
+  ingredients : Ingredient[];
+  private subcription : Subscription
+  constructor(private shoppingList:ShoppingListService) { }
+
+  ngOnInit() {
+    this.ingredients = this.shoppingList.getIngredients();
+    this.subcription = this.shoppingList.ingredientsChanged.subscribe(
+      (ingredients)=>{
+        this.ingredients = ingredients
+      }
+    )
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.shoppingList.addIngredients([ingredient])
+  }
+  ngOnDestroy(): void {
+    this.subcription.unsubscribe()
+  }
+}
---
+++ start/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,20 @@
+import { Subject } from "rxjs";
+import { Ingredient } from "../shared/ingredient.model";
+export class ShoppingListService{
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+    ingredientsChanged = new Subject<Ingredient[]>();
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+    addIngredient(ingredient:Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.next(this.ingredients.slice())
+    }
+    addIngredients(ingredients: Ingredient[]) {
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+}

9a00d7bfcc6e3fb678cfa842b202020f719529e9
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-14 11:32:00 +0700

    use types with http client

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,27 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post<{name:string}>('https://angularseries-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', postData)
+      .subscribe((responseData) => {
+        console.log(responseData)
+      })
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts()
   }
+  private fetchPosts() {
+    this.http
+      .get<{ [key:string] :Post }>('https://angularseries-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+      .pipe(
+        map((responeData) => {
+          const  arr = []
+          for (const keys in responeData) {
+            if (responeData.hasOwnProperty(keys)) {
+              const postData =  (responeData as any)[keys];
+              arr.push({  id:keys ,...postData})
+            }
+          }
+          console.log(arr)
+        })
+      )
+      .subscribe(posts => {
+      })
 
+  }
   onClearPosts() {
     // Send Http request
   }

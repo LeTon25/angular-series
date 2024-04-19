6128a77286164ea56e8c8762828719da7011bfc5
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-19 20:40:07 +0700

    end

--- standalone/src/app/app.component.ts
+++ standalone/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
+  standalone:true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
+
+bootstrapApplication(AppComponent)
--- standalone/src/app/welcome/welcome.component.ts
+++ standalone/src/app/welcome/welcome.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone:true,
+  imports:[WelcomeComponent],
   selector: 'app-welcome',
   templateUrl: './welcome.component.html'
 })

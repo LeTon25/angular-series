a87ef88b51a7cca542131045dd4484014645f484
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-04-19 15:28:52 +0700

    add auth module

--- auth/src/app/app.module.ts
+++ auth/src/app/app.module.ts
@@ -5,43 +5,30 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 
-import { RecipeService } from './recipes/recipe.service';
-import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner/loading-spinner.component';
-import { AuthInterceptor } from './auth/auth.interceptor';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipeModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
+    SharedModule,
     RecipeModule,
     ShoppingListModule,
-    SharedModule
+    CoreModule,
+    AuthModule
   ],
-  providers: [ShoppingListService, RecipeService,{
-    provide: HTTP_INTERCEPTORS,
-    useClass :AuthInterceptor,
-    multi:true
-  }],
   bootstrap: [AppComponent],
   
 })

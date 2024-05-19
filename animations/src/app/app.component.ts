3c850edd17f2c19964241acafc801c57b055ed73
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-05-19 11:14:51 +0700

    start

---
+++ animations/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations:[
+    trigger('divState',[])
+  ]
+})
+export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item) {
+      this.list.push(item);
+    }
+}

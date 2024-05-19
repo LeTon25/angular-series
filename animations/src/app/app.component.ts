59623baa081ee87ce798fc9c30357ed1ffe39a31
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-05-19 15:28:36 +0700

    Trigger and state

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { trigger } from '@angular/animations';
+import { trigger ,state, style} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   animations:[
-    trigger('divState',[])
+    trigger('divState',[
+      state("normal",style({
+        'background-color':'red',
+         transform:'translateX(0)'
+      })),
+      state("highlight",style({
+        'background-color':'blue',
+        transform:'translateX(100px)'
+      }))
+    ])
   ]
 })
 export class AppComponent {
+  state = 'normal'
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {

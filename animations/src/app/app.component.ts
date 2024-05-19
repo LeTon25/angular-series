1793569e8eeb48f584d2fa57ad52535ffd048e73
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-05-19 16:38:20 +0700

    transitions

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { trigger ,state, style} from '@angular/animations';
+import { trigger ,state, style, transition, animate} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -13,12 +13,54 @@ import { Component } from '@angular/core';
       state("highlight",style({
         'background-color':'blue',
         transform:'translateX(100px)'
-      }))
-    ])
+      })),
+      transition('normal <=> highlight',animate(300)),
+
+    ]),
+    trigger('wildState',[
+      state("normal",style({
+        'background-color':'red',
+         transform:'translateX(0) scale(1)'
+      })),
+      state("highlight",style({
+        'background-color':'blue',
+        transform:'translateX(100px) scale(1)'
+      })),
+      state("shrunked",style({
+        'background-color':'green',
+        transform:'translateX(100px) scale(0.5)'
+      })),
+      transition('normal <=> highlight',animate(300)),
+      transition('shrunked <=> *',[
+        style({'background-color' : 'orange'}),
+        animate(1000,style({borderRadius :'50px'})),
+        animate(500)
+      ]),
+    ]),
+    trigger('list1',[
+      state("in",style({
+        'opacity':1,
+         transform:'translateX(0)'
+      })),
+      transition('void => *',[
+        style({
+          opacity : 0,
+          transform :'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void',[
+        animate(300,style({
+          opacity : 0,
+          transform :'translateX(100px)'
+        }))
+      ])
+    ]),
   ]
 })
 export class AppComponent {
-  state = 'normal'
+  state = 'normal';
+  wildState ='normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
@@ -26,5 +68,12 @@ export class AppComponent {
     }
     onAnimate(){
       this.state = this.state =='normal' ? 'highlight' : 'normal';
+      this.wildState = this.wildState =='normal' ? 'highlight' : 'normal';
+    }
+    onShrunked(){
+      this.wildState  = 'shrunked';
+    }
+    onDelete(item) {
+      this.list.splice(this.list.indexOf(item), 1);
     }
 }
